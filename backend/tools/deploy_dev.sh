5fc76828f66e486c52461b158531113ef553bb48
Nguyễn Trang Tình Sử <nguyentrangtinhsu@gmail.com>
2026-01-27 19:35:20 +0700

    tools: repair deploy_dev.sh

--- backend/tools/deploy_dev.sh
+++ backend/tools/deploy_dev.sh
@@ -1,4 +1,3 @@
-#/usr/bin/env node /mnt/e/ubndxanuicam_deploy/frontend/node_modules/react-scripts/scripts/start.js bash
 set -euo pipefail
 # Simple deploy script for dev -> installs deps and restarts pm2 app
 BASEDIR=/mnt/e/ubndxanuicam_deploy/backend
